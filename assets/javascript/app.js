39e8bc2c012c7f419f5c1f5be5b360879eead3ca
Tyler Bumguardner <tylerbumguardner@gmail.com>
2018-10-11 19:58:01 -0700

    Added html css and js

---
+++ assets/javascript/app.js
@@ -0,0 +1,86 @@
+var time = 120;
+var timeInterval;
+var questions = [
+    {
+        question: "What color are aircraft black boxes?",
+        answers: ["Orange", "Yellow", "Black", "Green"],
+        answerIndex: 0
+    },
+    {
+        question: "In what state was Tennessee Williams born in?",
+        answers: ["Mississippi", "Tennessee", "Florida", "California"],
+        answerIndex: 0
+    },
+    {
+        question: "What kind of animal is a jackrabbit?",
+        answers: ["Rabbit", "Jackalope", "Hare", "Chupacabra"],
+        answerIndex: 2
+    },
+    {
+        question: "How long is New Zealands Ninety Mile Beach?",
+        answers: ["1 mile", "25 miles", "55 miles", "300 feet"],
+        answerIndex: 2
+    },
+    {
+        question: "Before Mount Everest was discovered, what was the tallest mountain?",
+        answers: ["Kilimanjaro", "Everest", "Matterhorn",],
+        answerIndex: 1
+    },
+    {
+        question: "In which month does Russia celebrate the October Revolution?",
+        answers: ["September", "October", "November", "March"],
+        answerIndex: 2
+    },
+];
+
+$("#timer").text(time);
+renderQuestions();
+
+$("#start").on("click", function () {
+    $("#intro").addClass("hidden");
+    $("#trivia").removeClass("hidden");
+
+    timeInterval = setInterval(function () {
+        time--;
+        if (time === 0) {
+            clearInterval(timeInterval);
+            checkTrivia();
+        }
+        $("#timer").text(time);
+    }, 1000);
+});
+
+function renderQuestions() {
+
+    questions.forEach(function (question, index) {
+        var $form = $("<form>");
+        var $question = $("<h3>").text(question.question);
+
+        $form.append($question);
+
+        question.answers.forEach(function (answer, i) {
+            var $input = $('<input type="radio">');
+            $input.attr("value", answer);
+            $input.attr("name", index);
+            $form.append($input);
+            $form.append(answer);
+        });
+
+        $("#questions").append($form);
+    });
+}
+
+function checkTrivia() {
+    var $forms = $("form");
+    $forms.each(function (i, elem) {
+        $(elem).find("input:checked").each(function (i, elem) {
+            console.log(elem);
+        });
+    })
+}
+
+$("#reset").on("click", function () {
+    console.log("hello")
+    time = 120;
+})
+
